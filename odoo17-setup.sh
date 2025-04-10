eed54c24d2b2af6ff41a4bdaf8866ffe58228ebf
Quang Trinh <trinhvanquangf1@gmail.com>
2025-04-10 17:18:18 +0700

    Update odoo17-setup.sh
    Add Nginx signing key to allow user install the latest version of nginx

--- odoo17-setup.sh
+++ odoo17-setup.sh
@@ -258,6 +258,23 @@ sudo systemctl start odoo17
 # Go to http://localhost:8069
 
 # Install Nginx and Configure
+# Install dependencies
+sudo apt install curl gnupg2 ca-certificates lsb-release ubuntu-keyring -y
+
+# Add Nginx signing key
+curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
+  | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
+
+# Set up the stable repository
+echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
+http://nginx.org/packages/ubuntu $(lsb_release -cs) nginx" \
+  | sudo tee /etc/apt/sources.list.d/nginx.list
+
+# Pin the official repo to take priority over Ubuntu default
+echo -e "Package: *\nPin: origin nginx.org\nPin-Priority: 900" \
+  | sudo tee /etc/apt/preferences.d/99nginx
+
+sudo apt update -y 
 sudo apt install nginx-core -y
 # config nginx for 
 
